55564b9a2677f9dfd619dca283442f20ea34b519
Igor Polevoy <500941+ipolevoy@users.noreply.github.com>
2020-03-27 17:37:27 -0500

    moving all examples to common repo

--- src/main/java/app/controllers/SecureController.java
+++
@@ -1,9 +0,0 @@
-package app.controllers;
-
-import app.controllers.authorization.Protected;
-import org.javalite.activeweb.AppController;
-
-@Protected
-public class SecureController extends AppController {
-    public void index(){}
-}
--- src/main/webapp/WEB-INF/views/public/index.ftl
+++
@@ -1,8 +0,0 @@
-<@content for="title">Public page</@content>
-
-<div class="row">
-    <div class="col-lg-4">
-        <h2>Public page</h2>
-        Hi, there. Public information here.
-    </div>
-</div>
